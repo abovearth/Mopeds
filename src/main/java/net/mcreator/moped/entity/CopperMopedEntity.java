5d791b9395e262a0749db22ba1cb20be04b7e657
abovearth <roeland_desmet@hotmail.com>
2024-05-03 16:55:01 +0200

    this code can seat a mob on a moped with a leash, but it doesn't use it's ai. That part I also tried to solve by copying the AI goals. It does not work. This current code crashes the game when a player gets of a moped (that a mob has ridden?).

--- src/main/java/net/mcreator/moped/entity/CopperMopedEntity.java
+++ src/main/java/net/mcreator/moped/entity/CopperMopedEntity.java
@@ -68,12 +68,32 @@ import net.mcreator.moped.procedures.CopperMopedEntityDiesProcedure;
 import net.mcreator.moped.init.MopedModEntities;
 //extra import to find the key item
 import net.mcreator.moped.init.MopedModItems;
+//extra imports for seating leashed animals
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.monster.Shulker;
+import com.google.common.base.Optional;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
 
 import javax.annotation.Nullable;
 import javax.annotation.Nonnull;
 
 import io.netty.buffer.Unpooled;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.WrappedGoal;
+import net.minecraft.server.commands.SayCommand;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
 
 public class CopperMopedEntity extends PathfinderMob implements GeoEntity {
 	public static final EntityDataAccessor<Boolean> SHOOT = SynchedEntityData.defineId(CopperMopedEntity.class, EntityDataSerializers.BOOLEAN);
@@ -127,6 +147,12 @@ public class CopperMopedEntity extends PathfinderMob implements GeoEntity {
 	public MobType getMobType() {
 		return MobType.UNDEFINED;
 	}
+	
+	@Override
+	public boolean canBeLeashed(Player p_21418_){
+		return false;
+	}
+
 
 	@Override
 	public boolean removeWhenFarAway(double distanceToClosestPlayer) {
@@ -246,13 +272,24 @@ public class CopperMopedEntity extends PathfinderMob implements GeoEntity {
 			return InteractionResult.sidedSuccess(this.level().isClientSide());
 		}
 		super.mobInteract(sourceentity, hand);
-		sourceentity.startRiding(this);
+		//sourceentity.startRiding(this);
+		
+		Level world = this.level();
+		
+		List<Entity> passengers = this.getPassengers();
+		if (!passengers.isEmpty() && passengers.get(0) instanceof Player)
+			return InteractionResult.PASS;
+		if (!world.isClientSide) {
+			this.ejectPassengers();
+			//sourceentity.startRiding(this);
+			//return InteractionResult.SUCCESS;	
+		}
+		sitDown(world, getLeashed(world, sourceentity).or(sourceentity)); //Removed pos,
  
 		double x = this.getX();
 		double y = this.getY();
 		double z = this.getZ();
 		Entity entity = this;
-		Level world = this.level();
 
 
 		return retval;
@@ -385,4 +422,94 @@ public class CopperMopedEntity extends PathfinderMob implements GeoEntity {
 	public AnimatableInstanceCache getAnimatableInstanceCache() {
 		return this.cache;
 	}
+
+	//Extra code to make mobs on a lead able to be seated on a moped
+	//This code is based on     Create/src/main/java/com/simibubi/create/content/contraptions/actors/seat/SeatBlock.java
+	public static boolean canBePickedUp(Entity passenger) {
+		if (passenger instanceof Shulker)
+			return false;
+		if (passenger instanceof Player)
+			return false;
+		/*if (AllEntityTags.IGNORE_SEAT.matches(passenger))
+			return false;*/ //this depends on a create specific library
+		/*if (!AllConfigs.server().logistics.seatHostileMobs.get() && !passenger.getType()
+			.getCategory()
+			.isFriendly())
+			return false;*/ //i don't have the create config
+		return passenger instanceof LivingEntity;
+	}
+
+	public static Optional<Entity> getLeashed(Level level, Player player) {
+		List<Entity> entities = level.getEntities((Entity) null, player.getBoundingBox() // removed player. before level
+			.inflate(10), e -> true);
+		for (Entity e : entities)
+			if (e instanceof Mob mob && mob.getLeashHolder() == player && CopperMopedEntity.canBePickedUp(e)) //changed SeeatBlock to CopperMopedEntity
+				return Optional.of(mob);
+		return Optional.absent();
+	}
+
+	public void sitDown(Level world, Entity entity) {//Removed  BlockPos pos,
+		if (world.isClientSide)
+			return;
+		/*SeatEntity seat = new SeatEntity(world, pos);
+		seat.setPos(pos.getX() + .5f, pos.getY(), pos.getZ() + .5f);
+		world.addFreshEntity(seat);*/
+		entity.startRiding(this, true); //replaced seat with this
+		if (entity instanceof TamableAnimal ta)
+			if (ta.isTame())
+				ta.setInSittingPose(true);
+		//Add goals from passenger to moped
+		if (entity instanceof Mob mob){
+			Set<WrappedGoal> goals = mob.goalSelector.getAvailableGoals();
+			Iterator<WrappedGoal> goalsIterator = goals.iterator();
+			while(goalsIterator.hasNext()) {
+				WrappedGoal wrappedGoalToAdd = goalsIterator.next();
+				//sayinchat(world, this.getX(), this.getY(), this.getZ(), wrappedGoalToAdd.getGoal().toString());
+				if (wrappedGoalToAdd.getFlags().contains(Goal.Flag.MOVE))
+	   				this.goalSelector.addGoal(wrappedGoalToAdd.getPriority(),wrappedGoalToAdd.getGoal());
+			}
+	   		Set<WrappedGoal> targetgoals = mob.targetSelector.getAvailableGoals();
+			Iterator<WrappedGoal> targetgoalsIterator = targetgoals.iterator();
+			while(targetgoalsIterator.hasNext()) {
+				WrappedGoal targetwrappedGoalToAdd = targetgoalsIterator.next();				
+				//sayinchat(world, this.getX(), this.getY(), this.getZ(), targetwrappedGoalToAdd.getGoal().toString());
+				if (targetwrappedGoalToAdd.getFlags().contains(Goal.Flag.MOVE))
+	   				this.goalSelector.addGoal(targetwrappedGoalToAdd.getPriority(),targetwrappedGoalToAdd.getGoal());
+			}
+		}
+	}
+	
+	@Override
+	protected void removePassenger(Entity entity) {
+		super.removePassenger(entity);
+		if (entity instanceof TamableAnimal ta)
+			ta.setInSittingPose(false);
+		//Remove goals from passenger to moped
+		
+		System.out.println("0");
+		Set<WrappedGoal> goals = this.goalSelector.getAvailableGoals();
+		System.out.println("1");
+		Iterator<WrappedGoal> goalsIterator = goals.iterator();
+		System.out.println("2");
+		while(goalsIterator.hasNext()) {
+			System.out.println("3");
+   			this.goalSelector.removeGoal(goalsIterator.next().getGoal());
+		}
+		
+		Set<WrappedGoal> targetgoals = this.targetSelector.getAvailableGoals();
+			Iterator<WrappedGoal> targetgoalsIterator = targetgoals.iterator();
+			while(targetgoalsIterator.hasNext()) {				
+	   			this.targetSelector.removeGoal(targetgoalsIterator.next().getGoal());
+			}
+		
+		
+	}
+	
+	public static void sayinchat(LevelAccessor world, double x, double y, double z, String message) {
+		if (message == null)
+			return;
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(), ("say " + message));
+	}
+
 }
